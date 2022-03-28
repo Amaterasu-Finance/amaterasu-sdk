b8c660c243256c9f491da31bbd3bd88637c14edf
FoxTheFarmer <FoxTheFarmer@cryptina.info>
2022-03-28 17:14:15 -0600

    update contracts

--- src/constants.ts
+++ src/constants.ts
@@ -32,7 +32,7 @@ export enum Rounding {
 export const FACTORY_ADDRESSES: { [chainId in ChainId]: string } = {
   [ChainId.MTV_MAINNET]: '0x6A4EA1056146Cd284B19Cd0FEE80Df3Dc80b9A90',
   [ChainId.AURORA_TESTNET]: '0x4eef2e729e235041d7f08D569f29fb83CABEF1c7',
-  [ChainId.AURORA_MAINNET]: '0x4eef2e729e235041d7f08D569f29fb83CABEF1c7'
+  [ChainId.AURORA_MAINNET]: '0x34696b6cE48051048f07f4cAfa39e3381242c3eD'
 }
 
 export const INIT_CODE_HASH = '0xb3da4698cc518015841d30e83d315ee7fe7123dc7cbbe30dc10538e10633deff'
@@ -47,7 +47,7 @@ export const THREE = JSBI.BigInt(3)
 export const FIVE = JSBI.BigInt(5)
 export const TEN = JSBI.BigInt(10)
 export const _100 = JSBI.BigInt(100)
-export const _997 = JSBI.BigInt(9975)
+export const _9975 = JSBI.BigInt(9975)
 export const _1000 = JSBI.BigInt(10000)
 
 export enum SolidityType {
